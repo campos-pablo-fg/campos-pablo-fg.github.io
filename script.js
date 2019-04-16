87f720ee196214c6550041376d399ca40022bcf8
user358 <user358@user358s-MacBook-Pro.local>
2019-04-16 22:32:59 +0300

    fixing js error

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
-var japanese_menu = document.getElementById("japanese_menu");
-var programming_menu = document.getElementById("programming_menu");
-var art_menu = document.getElementById("art_menu");
-var music_menu = document.getElementById("music_menu");
-var daily_menu = document.getElementById("daily_menu");
+const japanese_menu = document.getElementById("japanese_menu");
+const programming_menu = document.getElementById("programming_menu");
+const art_menu = document.getElementById("art_menu");
+const music_menu = document.getElementById("music_menu");
+const daily_menu = document.getElementById("daily_menu");
+const all_posts = document.getElementsByTagName("article");
 
 function showSpecificPosts(post_type){
-	var i;
-	var all_posts = document.getElementsByTagName("article");
-	for (i = 0; all_posts.length; i++) {
-		if (all_posts[i].className === post_type) {
- 			all_posts[i].hidden = false;
+	for (let i = 0; i < all_posts.length; i++) {
+			if (all_posts[i].className === post_type)
+			{
+				all_posts[i].hidden = false;
+			}
+			else
+			{
+				all_posts[i].hidden = true;
 		}
-		else{
-			all_posts[i].hidden = true;
-		}	
 	}
 }
-japanese_menu.addEventListener('click', () => {
+
+japanese_menu.addEventListener("click", function(){
 	showSpecificPosts("japanese");
 });
-programming_menu.addEventListener('click', () => {
+
+programming_menu.addEventListener('click', function(){
 	showSpecificPosts("programming");
 });
 
-art_menu.addEventListener('click', () => {
+art_menu.addEventListener('click', function(){
 	showSpecificPosts("art");
 });
 
-daily_menu.addEventListener('click', () => {
+daily_menu.addEventListener('click', function() {
 	showSpecificPosts("daily");
 });
 
-music_menu.addEventListener('click', () => {
+music_menu.addEventListener('click', function(){
 	showSpecificPosts("music");
 });
