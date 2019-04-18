c0561cdf0e4156a3f07f3686e871327af223bb70
user358 <user358@user358s-MacBook-Pro.local>
2019-04-19 00:50:10 +0300

    adding sort

--- script.js
+++ script.js
@@ -4,21 +4,40 @@ const art_menu = document.getElementById("art_menu");
 const music_menu = document.getElementById("music_menu");
 const daily_menu = document.getElementById("daily_menu");
 const all_posts = document.getElementsByTagName("article");
+const post_values = Object.values(all_posts);
+const main_wrap = document.getElementById("Main");
+let good_post_array = [];
 
-function showSpecificPosts(post_type){
-	for (let i = 0; i < all_posts.length; i++) {
-			if (all_posts[i].className === post_type)
-			{
-				all_posts[i].hidden = false;
-			}
-			else
-			{
-				all_posts[i].hidden = true;
-		}
+sortPost(post_values);
+
+function sortPost(array){
+
+	array.sort(function(a, b) {
+    return new Date(b.children[1].getAttribute("datetime")) - new Date(a.children[1].getAttribute("datetime"));
+	});
+	for(const order_post of array)
+	{
+		main_wrap.appendChild(order_post);
 	}
 }
 
+function showSpecificPosts(post_type){
+	good_post_array = [];
+	for (const post of post_values) {
+		if(post.className === post_type){
+			good_post_array.push(post);
+			main_wrap.removeChild(post);
+			post.hidden = false;
+		}
+		else{
+			post.hidden = true;
+		}
+	}
+	sortPost(good_post_array);
+}
+
 japanese_menu.addEventListener("click", function(){
+	//sortPost();
 	showSpecificPosts("japanese");
 });
 
